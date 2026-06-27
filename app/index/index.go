1143e2abc5c46a84593dc89e040c7e1beccc9341
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2026-06-26 18:33:56 -0700

    Update to use `whosonfirst/go-whosonfirst/v4` (#3)
    * snapshot: start v4 update
    
    * update vendor deps; incomplete
    
    * remove old deps
    
    * compiles, untested
    
    * run go fix
    
    * fix tests
    
    ---------
    
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- app/index/index.go
+++ app/index/index.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	_ "github.com/whosonfirst/go-whosonfirst-iterate-git/v3"
+	_ "github.com/whosonfirst/go-whosonfirst/v4/iterate/git"
 	_ "github.com/whosonfirst/spelunker/v2/app"
 	_ "github.com/whosonfirst/spelunker/v2/app/index/commands/opensearch"
 	_ "github.com/whosonfirst/spelunker/v2/app/index/commands/sql"
--- http/templates/funcs/funcs_test.go
+++ http/templates/funcs/funcs_test.go
@@ -10,7 +10,7 @@ func TestReplaceAll(t *testing.T) {
 
 	uris := http.DefaultURIs()
 
-	v := http.ReplaceAll(uris.Descendants, "{id}", int64(136251273))
+	v := http.URIForIdSimple(uris.Descendants, int64(136251273))
 
 	if v != "/id/136251273/descendants" {
 		t.Fatalf("Failed replacement")
